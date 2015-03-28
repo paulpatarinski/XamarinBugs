eeba7ebd6f890113a28b8335f51565f7a8e8fc56
Paul Patarinski <ppatarinski@paylocity.com>
2015-03-28 12:57:02 -0500

    Adding a Entry Renderer to fix a big with Android not setting Background color properly

---
+++ XamarinBugs/XamarinBugs/XamarinBugs.Droid/EntryRenderer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Xamarin.Forms;
+using Paylocity.Client.Android.Renderers;
+using Xamarin.Forms.Platform.Android;
+using System.ComponentModel;
+
+[assembly: ExportRenderer (typeof(Entry), typeof(PaylocityEntryRenderer))]
+
+namespace Paylocity.Client.Android.Renderers
+{
+	public class PaylocityEntryRenderer : EntryRenderer
+	{
+		public Entry FormsControl
+		{
+			get { return Element as Entry; }
+		}
+
+		public EntryEditText NativeControl
+		{
+			get { return Control; }
+		}
+
+		protected override void OnElementChanged (ElementChangedEventArgs<Entry> e)
+		{
+			base.OnElementChanged (e);
+
+			if (e.OldElement != null) return;
+
+			if (FormsControl != null)
+			{
+				NativeControl.SetBackgroundColor(Color.Transparent.ToAndroid ());
+			}
+		}
+
+		protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+		{
+			base.OnElementPropertyChanged(sender, e);
+			NativeControl.SetBackgroundColor(Color.Transparent.ToAndroid ());
+
+		}
+	}
+}
+
