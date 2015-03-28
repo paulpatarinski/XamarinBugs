7c087cb7f14b9ed9216989ba953189ef5574e1e2
Paul Patarinski <ppatarinski@paylocity.com>
2015-03-28 13:20:17 -0500

    Adding a picker renderer to fix an issue with the background color

---
+++ XamarinBugs/XamarinBugs/XamarinBugs.Droid/PickerRenderer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Xamarin.Forms;
+using Paylocity.Client.Android.Renderers;
+using Xamarin.Forms.Platform.Android;
+using System.ComponentModel;
+using Android.Widget;
+
+[assembly: ExportRenderer (typeof(Picker), typeof(CustomPickerRenderer))]
+
+namespace Paylocity.Client.Android.Renderers
+{
+	public class CustomPickerRenderer : PickerRenderer
+	{
+		public Picker FormsControl
+		{
+			get { return Element as Picker; }
+		}
+
+		public EditText NativeControl
+		{
+			get { return Control; }
+		}
+
+		protected override void OnElementChanged (ElementChangedEventArgs<Picker> e)
+		{
+			base.OnElementChanged (e);
+
+			if (e.OldElement != null) return;
+
+			if (FormsControl != null)
+			{
+				NativeControl.SetBackgroundColor(Color.Transparent.ToAndroid ());
+			}
+		}
+
+		protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+		{
+			base.OnElementPropertyChanged(sender, e);
+			NativeControl.SetBackgroundColor(Color.Transparent.ToAndroid ());
+
+		}
+	}
+}
+
